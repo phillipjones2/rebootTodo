344d1027086ff716047e972c15adf11752423297
Phillip Jones <phillipemersonjones@gmail.com>
2016-07-25 00:39:31 -0500

    edit and delete routes are done

--- routes/todos.js
+++ routes/todos.js
@@ -3,6 +3,7 @@ const express = require('express'),
      mongoose = require('mongoose'),
          Todo = require('../modules/todoModel');
 
+// todo index - all todos
 router.get('/', (req, res) => {
   Todo.find({
   }, (err, docs) => {
@@ -12,6 +13,16 @@ router.get('/', (req, res) => {
   });
 });
 
+// todo show - one todo -response is json
+router.get('/:todo_id', (req, res) => {
+  Todo.findById(req.params.todo_id, (err, todo) => {
+    if (err)
+      res.send(err);
+    res.send(todo);
+  });
+});
+
+// create todo
 router.post('/', (req, res) => {
   var todo = new Todo({ title: req.body.title, body: req.body.body});
   todo.save((err, doc) => {
@@ -20,6 +31,36 @@ router.post('/', (req, res) => {
     }, (err, docs) => {
       res.render('index', { title: 'Express', todosObj: docs });
     });
+  });
+});
+
+// edit todo
+router.put('/:todo_id', (req, res) => {
+  Todo.findById(req.params.todo_id, (err, todo) => {
+    if (err)
+      res.send(err);
+    todo.title = req.body.title;
+    todo.body = req.body.body;
+    todo.save((err, doc) => {
+      if (err) return consol.error(err);
+      Todo.find({
+      }, (err, docs) => {
+        res.render('index', { title: 'Express', todosObj: docs });
+      });
+    });
+  });
+});
+
+router.delete('/:todo_id', (req, res) => {
+  Todo.remove({_id: req.params.todo_id}, (err) => {
+    if (err)
+      res.send(err);
+    else {
+      Todo.find({
+      }, (err, docs) => {
+        res.render('index', { title: 'Express', todosObj: docs });
+      });
+    }
   });
 });
 
