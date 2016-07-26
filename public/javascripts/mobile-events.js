8ce6befb912a3e9b095b2ac5bcb92fc63e2ee77b
Phillip Jones <phillipemersonjones@gmail.com>
2016-07-26 16:41:01 -0500

    deleted the first attempt to delete todo after swiping right

--- public/javascripts/mobile-events.js
+++ public/javascripts/mobile-events.js
@@ -85,67 +85,6 @@ for (let todo of todos) {
     thisTodo.classList.remove('completed-todo');
     thisTodoTitle.classList.add('font-white');
 
-    // Insert delete confirmation string above title.
-
-    // Create necessary elements and text node.
-    // const confirmDeleteMessage = document.createElement('h2'),
-    //      confirmDeleteButtonDiv = document.createElement('div'),
-    //      confirmDeleteButton = document.createElement('img'),
-    //      cancelDeleteButton = document.createElement('img'),
-    //      deleteMessage = document.createTextNode('Delete todo?');
-    //
-    // // Add classes to confirmDeleteButtonDiv.
-    // addClasses(confirmDeleteButtonDiv, [
-    //   'box', 'all-12', 'space-inside'
-    // ]);
-    // let todoParentID = todo.getAttribute('id');
-    // // Give buttons respective image srcs.
-    // confirmDeleteButton.setAttribute('src', 'icons/checked.svg');
-    // confirmDeleteButton.setAttribute('todo-parent',todoParentID);
-    // addClasses(confirmDeleteButton, [
-    //     'confirmation-action-button',
-    //     'confirmation-delete'
-    //   ]);
-    //
-    // cancelDeleteButton.setAttribute('src', 'icons/cancel.svg');
-    // cancelDeleteButton.setAttribute('todo-parent',todoParentID)
-    //     addClasses(cancelDeleteButton, [
-    //     'confirmation-action-button',
-    //     'confirmation-cancel'
-    //   ]);
-    //
-    // // Place buttons inside of confirmDeleteButtonDiv.
-    // confirmDeleteButtonDiv.appendChild(cancelDeleteButton);
-    // confirmDeleteButtonDiv.appendChild(confirmDeleteButton);
-    // confirmDeleteButtonDiv.classList.add('pt_25');
-    //
-    // // Add text node to confirmDeleteMessage.
-    // confirmDeleteMessage.appendChild(deleteMessage);
-    //
-    // // Insert confirmDeleteMessage into the todo before the title.
-    // todo.insertBefore(confirmDeleteMessage, todo.tree.title);
-    //
-    // // Insert confirmDeleteButtonDiv at end of the todo.
-    // todo.appendChild(confirmDeleteButtonDiv);
-
-
-    // confirmDeleteButton.addEventListener('click', (e) => {
-    //   const todoTree = getParentTodo(confirmDeleteButton);
-    //         console.log(todoTree);
-      // const req = new XMLHttpRequest();
-    	// req.open('delete', putLink , true);
-    	// req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    	// req.send(`title=${todoTitle}&body=${todoBody}&priority=${priority}`);
-
-    	// setTimeout(() => {
-    	// 	location.reload();
-    	// }, 150);
-    // });
-
-    // cancelDeleteButton.addEventListener('click', (e) => {
-    //
-    // });
-
      } else { // If left swipe...
        if (thisTodo.classList.contains('deleted-todo')) return;
        if (thisTodo.classList.contains('completed-todo')) {
