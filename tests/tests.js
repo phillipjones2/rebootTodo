26fda7f2de2a6213fcd9877c9a8ccd71881b0228
Phillip Jones <phillipemersonjones@gmail.com>
2016-08-03 00:34:20 -0500

    deleted the tests folder.  the test folder remains

--- tests/tests.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-
-describe('Array', ( ) => {
-	describe('indexOf()', ( ) => {
-		it('should return -1 if given number is not present', ( ) => {
-			assert.equal(-1, [1,2,3].indexOf(4));
-		})
-	})
-})
