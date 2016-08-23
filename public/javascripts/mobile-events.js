d13637c8455a15489a840f0fdbaef39859d6be99
Phillip Jones <phillipemersonjones@gmail.com>
2016-08-22 23:15:32 -0500

    remove alert from isMobile function

--- public/javascripts/mobile-events.js
+++ public/javascripts/mobile-events.js
@@ -5,9 +5,6 @@ const isMobileDevice = (() => {
   } else { return false }
 })();
 
-alert(isMobileDevice);
-
-
  const mobileEvents = () => {
   const todos = getElsByClass('todo-box'),
     timestamp = new Date();
